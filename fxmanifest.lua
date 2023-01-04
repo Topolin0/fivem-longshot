7e51a4436109ff485b263a77fdace14b7e41eab2
Marco <52218760+Topolin0@users.noreply.github.com>
2023-01-05 00:25:04 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+version '1.0'
+
+author 'Topolin0'
+
+files {
+
+	"Weapons/*.meta",
+    "WeaponsMk2/*.meta",
+}
+
+data_file "WEAPONINFO_FILE_PATCH"   "WeaponsMk2/*.meta"
+data_file "WEAPONINFO_FILE_PATCH"   "Weapons/*.meta"
